3f1c5b294a9f6bbc238689b3b347803f5dfa16ba
Andrew Wood <23269446+andrewwoood@users.noreply.github.com>
2022-11-13 21:23:44 -0500

    vince plz call me when u get this

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from 'virtual:generated-pages'
+import App from './App.vue'
 
-import App from "./App.vue";
-import router from "./router";
+import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+import 'uno.css'
 
-import "./assets/main.css";
-
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+app.use(router)
+app.mount('#app')
