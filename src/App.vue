86310343ca8359f47afc5b4ed3dca871100f5e5e
Andrew Wood <23269446+andrewwoood@users.noreply.github.com>
2022-11-25 23:22:06 -0500

    well now we have a grid

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
       <h1>Memory Game</h1>
+      <div class="card-container">
+        <div v-for="n in 3">
+          <div class="card" v-for="n in 3">hello</div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
