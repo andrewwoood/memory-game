e01da1749e4401983b1ff9e11ccde8d01e96d00d
Andrew Wood <23269446+andrewwoood@users.noreply.github.com>
2022-12-26 16:27:25 -0700

    make divs clickable

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,19 @@
     <main font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
       <h1>Memory Game</h1>
       <div class="card-container">
-        <div class="card" v-for="n in 3*3">hello {{n}}</div>
+        <div @click="print(n)" class="card" v-for="n in 3*3">hello {{n}}</div>
       </div>
     </main>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    print(n) {
+      console.log(`hello from ${n}`);
+    }
+  }
+}
+</script>
